3db3d2f31b3f8ac6c395adce804ce97a42d2aefa
Filip-Mistyuk <mistukfilipp@gmail.com>
2024-07-17 00:13:00 -0700

    First css, fix main

--- Social/Social/urls.py
+++ Social/Social/urls.py
@@ -18,7 +18,6 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf.urls.static import static
 from django.conf import settings
-from django.conf.urls.i18n import i18n_patterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
--- Social/main/views.py
+++ Social/main/views.py
@@ -1,4 +1,4 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'main/home.html')
+    return render(request, 'base.html')
